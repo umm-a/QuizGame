954917b8f6495a26fab65135bdb20f0de6ec0b2a
Ling Wang <amyling.wang@gmail.com>
2022-11-28 11:47:56 +0100

    cleaned up

--- src/QuizGamev2/ServerListener.java
+++ src/QuizGamev2/ServerListener.java
@@ -26,8 +26,7 @@ public class ServerListener {
 
             }
         } finally {
-            ServerPlayer player = new ServerPlayer();
-            player.shutdown();
+            listener.close();
         }
 
     }
